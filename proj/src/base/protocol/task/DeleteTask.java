6c323965c39c84d4a634854d5ac08c3444292f2c
xRuiAlves <up201606746@fe.up.pt>
2019-04-14 23:48:22 +0100

    Refactored directories structure

--- proj/src/base/protocol/task/DeleteTask.java
+++ proj/src/base/protocol/task/DeleteTask.java
@@ -4,27 +4,25 @@ import base.channels.ChannelHandler;
 import base.channels.ChannelManager;
 import base.messages.CommonMessage;
 import base.messages.MessageFactory;
+import base.persistentstate.FileDeletionLog;
 import base.protocol.task.extendable.Task;
 
-public class RemovedTask extends Task {
-    private final int chunk_no;
-
-    public RemovedTask(String file_id, int chunk_no) {
+public class DeleteTask extends Task {
+    public DeleteTask(String file_id) {
         // This task doesn't need to retry sending
         super(file_id, false);
-        this.chunk_no = chunk_no;
+        FileDeletionLog.getInstance().addFile(file_id);
         prepareMessage();
     }
 
     @Override
     public void notify(CommonMessage msg) {
         // Do nothing, no replies to DELETE messages are predicted in the protocol
-        // TODO: Discuss possibility of extracting this method to an intermediate class
     }
 
     @Override
     protected byte[] createMessage() {
-        return MessageFactory.createRemovedMessage(this.file_id, this.chunk_no);
+        return MessageFactory.createDeleteMessage(this.file_id);
     }
 
     @Override
@@ -39,12 +37,12 @@ public class RemovedTask extends Task {
 
     @Override
     protected void printSendingMessage() {
-        System.out.printf("Sending REMOVED message for fileid '%s' and chunkno '%d' - attempt #%d\n", this.file_id, this.chunk_no, this.current_attempt + 1);
+        System.out.printf("Sending DELETE message for fileid '%s' - attempt #%d\n", this.file_id, this.current_attempt + 1);
     }
 
     @Override
     public String toKey() {
-        // "EXTRA_REMOVED_PREFIX" is added to ensure that there are no collisions with hashes of just file_ids and chunk_nos
-        return "EXTRA_REMOVED_PREFIX" + this.file_id + this.chunk_no;
+        // "EXTRA_DELETE_PREFIX" is added to ensure that there are no collisions with hashes of just file_ids
+        return "EXTRA_DELETE_PREFIX" + this.file_id;
     }
 }
--- proj/src/base/protocol/task/RemovedTask.java
+++ proj/src/base/protocol/task/RemovedTask.java
@@ -4,33 +4,31 @@ import base.channels.ChannelHandler;
 import base.channels.ChannelManager;
 import base.messages.CommonMessage;
 import base.messages.MessageFactory;
-import base.persistentstate.FileDeletionLog;
 import base.protocol.task.extendable.Task;
 
-public class DeleteTask extends Task {
-    public DeleteTask(String file_id) {
+public class RemovedTask extends Task {
+    private final int chunk_no;
+
+    public RemovedTask(String file_id, int chunk_no) {
         // This task doesn't need to retry sending
         super(file_id, false);
-        FileDeletionLog.getInstance().addFile(file_id);
+        this.chunk_no = chunk_no;
         prepareMessage();
     }
 
     @Override
     public void notify(CommonMessage msg) {
         // Do nothing, no replies to DELETE messages are predicted in the protocol
-        // TODO: Discuss possibility of extracting this method to an intermediate class
     }
 
     @Override
     protected byte[] createMessage() {
-        return MessageFactory.createDeleteMessage(this.file_id);
+        return MessageFactory.createRemovedMessage(this.file_id, this.chunk_no);
     }
 
     @Override
     protected void handleMaxRetriesReached() {
         this.unregister();
-        //TODO: Delete later, only being used for debug printing
-        System.out.printf("DELETE for fileid '%s' reached the maximum number of retries\n", this.file_id);
     }
 
     @Override
@@ -40,12 +38,12 @@ public class DeleteTask extends Task {
 
     @Override
     protected void printSendingMessage() {
-        System.out.printf("Sending DELETE message for fileid '%s' - attempt #%d\n", this.file_id, this.current_attempt + 1);
+        System.out.printf("Sending REMOVED message for fileid '%s' and chunkno '%d' - attempt #%d\n", this.file_id, this.chunk_no, this.current_attempt + 1);
     }
 
     @Override
     public String toKey() {
-        // "EXTRA_DELETE_PREFIX" is added to ensure that there are no collisions with hashes of just file_ids
-        return "EXTRA_DELETE_PREFIX" + this.file_id;
+        // "EXTRA_REMOVED_PREFIX" is added to ensure that there are no collisions with hashes of just file_ids and chunk_nos
+        return "EXTRA_REMOVED_PREFIX" + this.file_id + this.chunk_no;
     }
 }
